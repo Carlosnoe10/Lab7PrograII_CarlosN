3eddc4f02c9dfe1cbfb457f3f297ee934633bfe1
Carlosnoe10 <carlosdnoem@gmail.com>
2023-09-08 12:39:32 -0700

    INICIO 👌

--- src/lab8prograii_carlosnoé/ShadowBan.java
+++ src/lab8prograii_carlosnoé/ShadowBan.java
@@ -32,11 +32,11 @@ public class ShadowBan extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1307, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 748, Short.MAX_VALUE)
         );
 
         pack();
